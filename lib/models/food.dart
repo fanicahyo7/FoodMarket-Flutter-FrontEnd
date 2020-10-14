cadc4aa111e2e0c11aa9a2a2c9353b6d1db3c518
fanidc7 <fanidc7@gmail.com>
2020-10-14 09:45:53 +0700

    food card horizontal

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -13,11 +13,29 @@ class Food {
       this.price, this.rate);
 }
 
-Food mockFood = Food(
-    1,
-    "https://www.wartabromo.com/wp-content/uploads/WhatsApp-Image-2019-07-05-at-7.47.09-AM.jpeg",
-    "Bakso Malang",
-    "Bakso ini terbuat dari daging sapi berkualitas dan sangat terkenal di Kota Malang.",
-    "Daging Sapi",
-    12000,
-    4.2);
+List<Food> mockFoods = [
+  Food(
+      1,
+      "https://www.wartabromo.com/wp-content/uploads/WhatsApp-Image-2019-07-05-at-7.47.09-AM.jpeg",
+      "Bakso Malang",
+      "Bakso ini terbuat dari daging sapi berkualitas dan sangat terkenal di Kota Malang.",
+      "Daging Sapi",
+      12000,
+      4.2),
+  Food(
+      2,
+      "https://kbu-cdn.com/dk/wp-content/uploads/sate-kakul.jpg",
+      "Sate Madura",
+      "Sate Ayam asli Madura. Rasanya enak dan bikin ketagihan",
+      "Ayam, Bumbu Kacang",
+      10000,
+      4.5),
+  Food(
+      3,
+      "https://1.bp.blogspot.com/-cEQ1uvxECJU/XQcJ1WBhf3I/AAAAAAAADGE/6arTC6DlGqsfTwWvHPqQ7t6IpxHce5vcQCLcBGAs/s1600/Sempol.jpg",
+      "Sempol Ayam",
+      "Sempol Ayam. Rasanya enak dan bikin ketagihan",
+      "Ayam",
+      10000,
+      4.5),
+];
--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -48,7 +48,22 @@ class _FoodPageState extends State<FoodPage> {
               ),
             ),
             //food
-            FoodCard(mockFood)
+            Container(
+                height: 258,
+                width: double.infinity,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Row(
+                        children: mockFoods.map((e) => Padding(
+                          padding: EdgeInsets.only(
+                            left: (e == mockFoods.first) ? defaultMargin : 0,
+                            right: defaultMargin
+                          ),
+                          child: FoodCard(e),
+                        )).toList())
+                  ],
+                ))
             //food tab
           ],
         )
