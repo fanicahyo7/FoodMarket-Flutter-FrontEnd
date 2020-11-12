17aa2147db5dd364b22bc80651b10fb2789bbfaf
fanicahyo7 <fanidc7@gmail.com>
2020-11-12 20:58:02 +0700

    transaction

--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -1,16 +1,39 @@
 part of 'services.dart';
 
 class TransactionServices {
-  static Future<ApiReturnValue<List<Transaction>>> getTransactions() async {
-    await Future.delayed(Duration(seconds: 3));
+  static Future<ApiReturnValue<List<Transaction>>> getTransactions(
+      {http.Client client}) async {
+    if (client == null) {
+      client = http.Client();
+    }
 
-    return ApiReturnValue(value: moskTransactions);
+    String url = baseURL + "transaction";
+
+    var response = await client.get(url, headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer ${User.token}"
+    });
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Please try again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    List<Transaction> transactions = (data['data']['data'] as Iterable)
+        .map((e) => Transaction.fromJson(e))
+        .toList();
+
+    return ApiReturnValue(value: transactions);
   }
 
-  static Future<ApiReturnValue<Transaction>> submitTransaction(Transaction transaction) async {
+  static Future<ApiReturnValue<Transaction>> submitTransaction(
+      Transaction transaction) async {
     await Future.delayed(Duration(seconds: 2));
 
     // return ApiReturnValue(message: "Transaksi Gagal");
-    return ApiReturnValue(value: transaction.copyWith(id: 123, status: TransactionStatus.pending));
+    return ApiReturnValue(
+        value:
+            transaction.copyWith(id: 123, status: TransactionStatus.pending));
   }
 }
